e540c319849617e0c4ec223b7ddf0a61d6497cd6
rcanfield <Rcanfield86@gmail.com>
2024-05-11 14:02:42 -0400

    Use MF runtime for loading remotes in host

--- host/src/bootstrap.tsx
+++ host/src/bootstrap.tsx
@@ -1,5 +1,11 @@
-import App from './_app';
+import App from "./_app";
 import * as ReactDOMClient from "react-dom/client";
+import { init } from "@module-federation/enhanced/runtime";
+
+init({
+	name: "container",
+	remotes: [],
+});
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement!);
